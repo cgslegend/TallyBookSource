c4438d7ee2e4c0a93be28f9233f85316a69c2645
cgslegend <718827641@qq.com>
2022-01-27 04:37:42 +0800

    创建了icon组件并添加到页面中

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+import Icon from "./icon";
+
 
 const NavWapper = styled.nav`
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -8,7 +10,15 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
     >li{
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
   line-height: 24px;
@@ -19,13 +29,15 @@ const Nav = () =>{
     <NavWapper>
     <ul>
     <li>
+        <Icon name="tags" />
     <Link to="/tags">标签页</Link>
 </li>
     <li>
-
+        <Icon name="money" />
         <Link to="/money">记账页</Link>
     </li>
     <li>
+        <Icon name="statistics" />
         <Link to="/statistics">统计页</Link>
     </li>
 </ul>
