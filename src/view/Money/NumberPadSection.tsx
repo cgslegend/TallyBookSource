5894d4058bfe8d9a6573c63eb76b409ca1c3e6ef
cgslegend <718827641@qq.com>
2022-01-30 04:45:50 +0800

    完成 计算器的输入和数字显示 的功能

--- src/view/Money/NumberPadSection.tsx
+++ src/view/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
 >.outputNum{
@@ -50,4 +51,69 @@ const NumberPadSection = styled.section`
     }
   }
 `;
+
+const NumberPadSection:React.FC = () => {
+    const [output,setOutput] = useState('0');
+    const padTouch = (e:React.MouseEvent) =>{
+        const text = (e.target as HTMLButtonElement).textContent;
+        if (text === null){return}
+        switch (text) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (output.length >=16){return;}
+                else {
+                if (output === '0') {
+                    setOutput(text);
+                } else {
+                    setOutput(output + text);
+                }
+                break;}
+            case '删除' :
+                if (output.length === 1){
+                    setOutput('0');
+                }else {
+                    setOutput(output.slice(0,output.length-1));
+                }
+                break;
+            case '清空' :
+                setOutput('0')
+                break;
+            case 'OK' :
+                console.log('OK')
+                break;
+            case '.':
+                if (output.indexOf('.')>0){return;}
+                else {setOutput(output+'.')}
+                break;
+        }
+    }
+    return(
+        <Wrapper>
+            <div className="outputNum">{output}</div>
+            <div className="pad clearfix" onClick={padTouch}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className="OK">OK</button>
+                <button className="zero">0</button>
+                <button>.</button>
+            </div>
+        </Wrapper>)
+}
 export {NumberPadSection}
