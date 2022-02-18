baf941a0f3941ecbcea6fe662247fef2296c7fa3
cgslegend <718827641@qq.com>
2022-02-19 01:51:49 +0800

    优化样式

--- src/view/Money/TagSection.tsx
+++ src/view/Money/TagSection.tsx
@@ -8,9 +8,10 @@ const Wrapper = styled.section`
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
-  min-height: 240px;
   justify-content: flex-end;
+  align-items: flex-start;
+  min-height: 240px;
+  
 >ol{
   margin: 0 -12px;
   >li{
